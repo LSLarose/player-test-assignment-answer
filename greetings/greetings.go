1c6de5576e7b09027bf237a56c0372254e569f3e
Louis Sérey Larose <louiss.larose@gmail.com>
2021-05-02 21:19:02 -0400

    added error handling, renamed hello.go => main.go

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,14 +1,20 @@
 package greetings
 
 import (
+	"errors"
 	"fmt"
 
 	"rsc.io/quote/v3"
 )
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome! Did you know that \"%v\"", name, quote.GoV3())
-	return message
+	return message, nil
 }
